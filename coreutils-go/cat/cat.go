5a90668b7d1d3c86898e9380b2f5629347315f38
okkun-sh <okkun.sh@gmail.com>
2023-09-21 00:55:03 +0900

    feat: add option -n to cat command

--- coreutils-go/cat/cat.go
+++ coreutils-go/cat/cat.go
@@ -1,41 +1,61 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"strings"
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	n := flag.Bool("n", false, "number the output lines")
+	flag.Parse()
+
+	if len(flag.Args()) < 1 {
 		fmt.Println("Missing file name")
 		os.Exit(1)
 		return
 	}
 
-	if err := run(os.Args[1]); err != nil {
+	if err := run(flag.Args()[0], *n); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 		return
 	}
 }
 
-func run(path string) error {
-	s, err := read(path)
+func run(path string, b bool) error {
+	s, err := read(path, b)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(s)
+	fmt.Print(s)
 
 	return nil
 }
 
-func read(path string) (string, error) {
+func read(path string, n bool) (string, error) {
 	data, err := os.ReadFile(path)
 	if err != nil {
 		return "", fmt.Errorf("Error reading file %s: %v", path, err)
 	}
 
-	return strings.TrimRight(string(data), "\n"), nil
+	var content string
+	if n {
+		var builder strings.Builder
+		scanner := bufio.NewScanner(strings.NewReader(string(data)))
+		num := 1
+		for scanner.Scan() {
+			line := scanner.Text()
+			fmt.Fprintf(&builder, "%6d\t%s\n", num, line)
+			num++
+		}
+		content = builder.String()
+	} else {
+		content = string(data)
+	}
+
+	return content, nil
 }
--- coreutils-go/cat/cat_test.go
+++ coreutils-go/cat/cat_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os/exec"
 	"path/filepath"
 	"testing"
 )
@@ -9,18 +10,27 @@ func TestRead(t *testing.T) {
 	tests := []struct {
 		name     string
 		filePath string
+		opt      bool
 		want     string
 		wantErr  bool
 	}{
 		{
 			name:     "display file content",
 			filePath: filepath.Join("testdata", "test.txt"),
-			want:     "hoge\nbar\nfoo",
+			opt:      false,
+			want:     cmdExec(t, filepath.Join("testdata", "test.txt"), ""),
+			wantErr:  false,
+		},
+		{
+			name:     "display file content without blank lines",
+			opt:      true,
+			filePath: filepath.Join("testdata", "blank_line.txt"),
+			want:     cmdExec(t, filepath.Join("testdata", "blank_line.txt"), "-n"),
 			wantErr:  false,
 		},
 		{
 			name:     "file not found",
-			filePath: "testdata/notfound.txt",
+			filePath: filepath.Join("testdata", "notfound.txt"),
 			wantErr:  true,
 		},
 	}
@@ -28,7 +38,7 @@ func TestRead(t *testing.T) {
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			s, err := read(tt.filePath)
+			s, err := read(tt.filePath, tt.opt)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("read() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -38,4 +48,20 @@ func TestRead(t *testing.T) {
 			}
 		})
 	}
+}
+
+func cmdExec(t *testing.T, path string, opt string) string {
+	var cmd *exec.Cmd
+	if opt != "" {
+		cmd = exec.Command("cat", opt, path)
+	} else {
+		cmd = exec.Command("cat", path)
+	}
+
+	out, err := cmd.Output()
+	if err != nil {
+		t.Error(err)
+	}
+
+	return string(out)
 }
